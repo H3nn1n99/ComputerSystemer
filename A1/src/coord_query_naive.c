a5d9d8036eb444521e7a0c03644a82f4ca65c325
nunus321 <jonasalmersal@outlook.dk>
2024-10-06 03:21:09 +0200

    coord_query_naive.c

--- A1/src/coord_query_naive.c
+++ A1/src/coord_query_naive.c
@@ -5,6 +5,7 @@
 #include <stdint.h>
 #include <errno.h>
 #include <assert.h>
+#include <math.h>
 
 #include "record.h"
 #include "coord_query.h"
@@ -14,19 +15,39 @@ struct naive_data {
   int n;
 };
 
+
+double distance(double x1, double y1, double x2, double y2) {
+    return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+}
+
 struct naive_data* mk_naive(struct record* rs, int n) {
-  assert(0);
-  // TODO
+  struct naive_data* data = malloc(sizeof(struct naive_data));
+  if (!data) {
+      fprintf(stderr, "Memory allocation failed\n");
+      exit(1);
+  }
+
+  data->rs = rs; 
+  data->n = n;   
+
+  return data;
 }
 
 void free_naive(struct naive_data* data) {
-  assert(0);
-  // TODO
+  free(data); 
 }
 
 const struct record* lookup_naive(struct naive_data *data, double lon, double lat) {
-  assert(0);
-  // TODO
+  const struct record* closest_record = NULL;
+  double min_distance = -1;
+  for (int i = 0; i < data->n; i++) {
+    double dist = distance(data->rs[i].lon, data->rs[i].lat, lon, lat); 
+    if (min_distance == -1 || dist < min_distance) {
+        min_distance = dist;
+        closest_record = &data->rs[i];
+    }
+  }
+  return closest_record;  
 }
 
 int main(int argc, char** argv) {
